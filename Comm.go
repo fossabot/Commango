27e5bf770299fda5893150fadc21a5aade0d6f99
Ximidar <ximidar@gmail.com>
2018-05-27 01:05:15 -0700

    Added more code for reading the serial port

--- Comm.go
+++ Comm.go
@@ -5,21 +5,24 @@
 * @Last Modified time: 2018-05-25 16:53:42
 */
 
-package main
+package comm
 
 import(
     "fmt"
     "github.com/jacobsa/go-serial/serial"
     "io"
+    "strings"
+    "encoding/hex"
 )
 
 type(
-    Comm_Interface Interface{
+    Comm_Interface interface{
         Open_Comm() (err error)
         Close_Comm() (err error)
-        Write_Comm(message string) (err error)
-        Write_Comm(message []string) (err error)
-        Read_Comm() (message string, err error)
+        Write_Comm_String(message string) (err error)
+        Write_Comm_Array(message []string) (err error)
+        //Read_Comm() (message string, err error)
+        Read_Line() (message string, err error)
     }
     Comm struct{
         options serial.OpenOptions
@@ -35,36 +38,70 @@ func New_Comm() (*Comm){
     return comm
 }
 
-func (comm *Comm) Init_Comm(port_path string, baud int, data_bits int, stop_bits int, minimum_read int) (err error){
+func (comm *Comm) Init_Comm(port_path string, baud uint, data_bits uint, stop_bits uint, minimum_read uint) (err error){
     //TODO explore other serial.OpenOptions
     comm.options = serial.OpenOptions{
         PortName: port_path,
         BaudRate: baud,
         DataBits: data_bits,
         StopBits: stop_bits,
-        MinimumReadSize: minimum_read
+        MinimumReadSize: minimum_read,
     }
 
     return 
 }
 
 func (comm *Comm) Open_Comm() (err error){
-
+    fmt.Printf("Opening port with address %v\n", comm.options.PortName)
+    comm.port, err = serial.Open(comm.options)
+    if err != nil {
+      fmt.Println("Error Could not open port", err)
+    }
     return
 }
 
 func (comm *Comm) Close_Comm() (err error){
+    fmt.Printf("Closing port with address %s\n", comm.options.PortName)
+    comm.port.Close()
     return
 }
 
-func (comm *Comm) Write_Comm(message string) (err error){
+func (comm *Comm) Write_Comm_String(message string) (err error){
+    byte_message := []byte(message)
+    _, err = comm.port.Write(byte_message)
+    if err == nil{
+        comm.Last_Write = message
+    }
+
     return
 }
 
-func (comm *Comm) Write_Comm(message []string) (err error){
+func (comm *Comm) Write_Comm_Array(message []string) (err error){
+    joined_message := strings.Join(message, " ")
+    err = comm.Write_Comm_String(joined_message)
     return
 }
 
-func (comm *Comm) Read_Comm() (message string, err error){
+func (comm *Comm) Read_Line() (message string, err error){
+    var read_bytes []byte
+    current_char := make([]byte, 1)
+    _, err = comm.port.Read(current_char)
+    if err != nil{
+        fmt.Printf("Could not read from %s", comm.options.PortName)
+        return
+    }
+    read_bytes = append(read_bytes, current_char[0])
+    for hex.EncodeToString(current_char) != "\n"{
+        current_char := make([]byte, 1)
+        _, err = comm.port.Read(current_char)
+        if err != nil{
+            if err == io.EOF{
+                fmt.Println("End of file reached")
+                break
+            }
+        }
+        read_bytes = append(read_bytes, current_char[0])
+    }
+    message = hex.EncodeToString(read_bytes)
     return
 }
