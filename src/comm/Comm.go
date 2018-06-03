4c8045a3c01546fcfd938f9941f6e1528649e928
Ximidar <ximidar@gmail.com>
2018-06-03 00:10:57 -0700

    ran go fmt on source code

--- src/comm/Comm.go
+++ src/comm/Comm.go
@@ -2,138 +2,136 @@
 * @Author: matt
 * @Date:   2018-05-25 15:58:30
 * @Last Modified by:   Ximidar
-* @Last Modified time: 2018-06-02 22:45:22
+* @Last Modified time: 2018-06-03 00:09:12
  */
 
 package commango
 
 import (
-    _"encoding/hex"
-    "fmt"
-    "github.com/jacobsa/go-serial/serial"
-    "io"
-    "strings"
-    "time"
-    "errors"
+	_ "encoding/hex"
+	"errors"
+	"fmt"
+	"github.com/jacobsa/go-serial/serial"
+	"io"
+	"strings"
+	"time"
 )
 
 type (
-    Comm struct {
-        options    serial.OpenOptions
-        Last_Read  string
-        Last_Write string
+	Comm struct {
+		options    serial.OpenOptions
+		Last_Read  string
+		Last_Write string
 
-        finished_reading bool
-        Port             io.ReadWriteCloser
-        
-    }
+		finished_reading bool
+		Port             io.ReadWriteCloser
+	}
 )
 
 func New_Comm() *Comm {
-    comm := new(Comm)
-    return comm
+	comm := new(Comm)
+	return comm
 }
 
 func (comm *Comm) Init_Comm(port_path string, baud uint, data_bits uint, stop_bits uint) (err error) {
-    //TODO explore other serial.OpenOptions
-    comm.options = serial.OpenOptions{
-        PortName:              port_path,
-        BaudRate:              baud,
-        DataBits:              data_bits,
-        StopBits:              stop_bits,
-        MinimumReadSize:       0,
-        InterCharacterTimeout: 1000,
-        ParityMode:            serial.PARITY_NONE,
-    }
+	//TODO explore other serial.OpenOptions
+	comm.options = serial.OpenOptions{
+		PortName:              port_path,
+		BaudRate:              baud,
+		DataBits:              data_bits,
+		StopBits:              stop_bits,
+		MinimumReadSize:       0,
+		InterCharacterTimeout: 1000,
+		ParityMode:            serial.PARITY_NONE,
+	}
 
-    return
+	return
 }
 
 func (comm *Comm) Open_Comm() (err error) {
-    fmt.Printf("Opening port with address %v\n", comm.options.PortName)
-    comm.Port, err = serial.Open(comm.options)
-    if err != nil {
-        fmt.Println("Error Could not open port", err)
-    }
-    // Sleep to allow the port to start up
-    time.Sleep(3 * time.Second)
-    return
+	fmt.Printf("Opening port with address %v\n", comm.options.PortName)
+	comm.Port, err = serial.Open(comm.options)
+	if err != nil {
+		fmt.Println("Error Could not open port", err)
+	}
+	// Sleep to allow the port to start up
+	time.Sleep(20 * time.Millisecond)
+	return
 }
 
 func (comm *Comm) Close_Comm() (err error) {
-    fmt.Printf("Closing port with address %s\n", comm.options.PortName)
-    comm.Port.Close()
-    return
+	fmt.Printf("Closing port with address %s\n", comm.options.PortName)
+	comm.Port.Close()
+	return
 }
 
 func (comm *Comm) Write_Comm_String(message string) (len_written int, err error) {
-    log_message := strings.Replace(message, "\n", "", -1)
-    log_message = fmt.Sprintf("SENT: %v", log_message)
-    fmt.Println(log_message)
-    byte_message := []byte(message)
-    expected_write := len(byte_message)
-    len_written, err = comm.Port.Write(byte_message)
-    if err == nil {
-        comm.Last_Write = message
-    }
-    if len_written != expected_write {
-        fmt.Println("Didn't write expected amount of bytes")
-        fmt.Printf("Written: %v Expected: %v", len_written, expected_write)
-    }
-    return
+	log_message := strings.Replace(message, "\n", "", -1)
+	log_message = fmt.Sprintf("SENT: %v", log_message)
+	fmt.Println(log_message)
+	byte_message := []byte(message)
+	expected_write := len(byte_message)
+	len_written, err = comm.Port.Write(byte_message)
+	if err == nil {
+		comm.Last_Write = message
+	}
+	if len_written != expected_write {
+		fmt.Println("Didn't write expected amount of bytes")
+		fmt.Printf("Written: %v Expected: %v", len_written, expected_write)
+	}
+	return
 }
 
 func (comm *Comm) Write_Comm_Array(message []string) (len_written int, err error) {
-    joined_message := strings.Join(message, " ")
-    len_written, err = comm.Write_Comm_String(joined_message)
-    return
+	joined_message := strings.Join(message, " ")
+	len_written, err = comm.Write_Comm_String(joined_message)
+	return
 }
 
-
 func (comm *Comm) ReadLine() (out []byte, err error) {
-    read_line := false
-    for read_line == false {
-        read, err := comm.ReadWithTimeout(1)
+	read_line := false
+	for read_line == false {
+		read, err := comm.ReadWithTimeout(1)
 
-        if err != nil {
-            //fmt.Println("Readline errored out", err)
-            return nil, err
-        }
+		if err != nil {
+			//fmt.Println("Readline errored out", err)
+			return nil, err
+		}
 
-        if read[0] == 10 {
-            out = append(out, read[0])
-            read_line = true
-            return out, nil
-        }
+		if read[0] == 10 {
+			out = append(out, read[0])
+			read_line = true
+			return out, nil
+		}
 
-        out = append(out, read[0])
+		out = append(out, read[0])
 
-    }
+	}
 
-    return
+	return
 
 }
 
 func (comm *Comm) ReadWithTimeout(n int) ([]byte, error) {
-    buf := make([]byte, n)
-    done := make(chan error)
-    readAndCallBack := func() {
-        _, err := io.ReadAtLeast(comm.Port, buf, n)
-        done <- err
-    }
+	buf := make([]byte, n)
+	done := make(chan error)
+	readAndCallBack := func() {
+		_, err := io.ReadAtLeast(comm.Port, buf, n)
+		done <- err
+	}
 
-    go readAndCallBack()
+	go readAndCallBack()
 
-    timeout := make(chan bool)
-    sleepAndCallBack := func() { time.Sleep(2e9); timeout <- true }
-    go sleepAndCallBack()
+	timeout := make(chan bool)
+	sleepAndCallBack := func() { time.Sleep(2e9); timeout <- true }
+	go sleepAndCallBack()
 
-    select {
-    case err := <-done:
-        return buf, err
-    case <-timeout:
-        return nil, errors.New("Timed out.")
-    }
+	select {
+	case err := <-done:
+		return buf, err
+	case <-timeout:
+		return nil, errors.New("Timed out.")
+	}
 
-    return nil, errors.New("Can't get here.")
+	return nil, errors.New("Can't get here.")
 }
--- src/main.go
+++ src/main.go
@@ -2,98 +2,94 @@
 * @Author: Ximidar
 * @Date:   2018-05-27 17:44:35
 * @Last Modified by:   Ximidar
-* @Last Modified time: 2018-06-02 23:28:26
+* @Last Modified time: 2018-06-03 00:09:45
  */
 
 package main
 
 import (
-    "fmt"
-    "github.com/Commango/src/comm"
-    "io"
-    "os"
-    "time"
-    "strings"
+	"fmt"
+	"github.com/Commango/src/comm"
+	"io"
+	"os"
+	"strings"
+	"time"
 )
 
 var COMM_OPEN bool
 
 func main() {
 
-    comm := commango.New_Comm()
+	comm := commango.New_Comm()
 
-    err := comm.Init_Comm("/dev/ttyACM0", 115200, 8, 1)
+	err := comm.Init_Comm("/dev/ttyACM0", 115200, 8, 1)
 
-    if err != nil {
-        fmt.Println(err)
-        fmt.Println("Cannot assign options")
-        os.Exit(2)
-    }
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("Cannot assign options")
+		os.Exit(2)
+	}
 
-    err = comm.Open_Comm()
-    COMM_OPEN = true
+	err = comm.Open_Comm()
+	COMM_OPEN = true
 
-    if err != nil {
-        fmt.Println("Cannot open port")
-    }
-    defer comm.Close_Comm()
+	if err != nil {
+		fmt.Println("Cannot open port")
+	}
+	defer comm.Close_Comm()
 
+	go Read_Forever(comm)
+	go Write_Forever(comm)
 
-    go Read_Forever(comm)
+	time.Sleep(2 * time.Second)
+	COMM_OPEN = false
+	time.Sleep(2 * time.Second)
 
-    go Write_Forever(comm)
-
-    fmt.Println("Sleeping")
-    time.Sleep(2 * time.Second)
-    COMM_OPEN = false
-    
-    fmt.Println("Finished")
+	fmt.Println("Finished")
 
 }
 
-func Read_Forever(comm *commango.Comm){
+func Read_Forever(comm *commango.Comm) {
 
-    for COMM_OPEN{
-        out, err := comm.ReadLine()
-        if err != nil{
-            if err != io.EOF{
-                fmt.Printf("%v", err)
-            } 
-        } else {
-            string_out := string(out)
-            string_out = strings.Replace(string_out, "\n", "", -1)
+	for COMM_OPEN {
+		out, err := comm.ReadLine()
+		if err != nil {
+			if err != io.EOF {
+				fmt.Printf("%v", err)
+			}
+		} else {
+			string_out := string(out)
+			string_out = strings.Replace(string_out, "\n", "", -1)
 
-            
-            if !check_blank(out){
-                string_out = fmt.Sprintf("RECV: %v", string_out)
-                fmt.Println(string_out)
-            }
-            
-        }
-    }
-    fmt.Println("Stopping the reading")
+			if !check_blank(out) {
+				string_out = fmt.Sprintf("RECV: %v", string_out)
+				fmt.Println(string_out)
+			}
+
+		}
+	}
+	fmt.Println("Stopping the reading")
 }
 
 func check_blank(byte_slice []byte) bool {
-    if len(byte_slice) > 2{
-        return false
-    }
+	if len(byte_slice) > 2 {
+		return false
+	}
 
-    return true
-    
+	return true
+
 }
 
-func Write_Forever(comm *commango.Comm){
-    count := 0
-    for COMM_OPEN{
-        message := fmt.Sprintf("Hello at count: %v\n", count)
-        _, err := comm.Write_Comm_String(message)
-        if err != nil {
-            fmt.Println(err)
-        } 
-        count += 1
-        time.Sleep(25 * time.Millisecond)
-    }
-    fmt.Println("Stopping the writing")
+func Write_Forever(comm *commango.Comm) {
+	count := 0
+	for COMM_OPEN {
+		message := fmt.Sprintf("Hello at count: %v\n", count)
+		_, err := comm.Write_Comm_String(message)
+		if err != nil {
+			fmt.Println(err)
+		}
+		count += 1
+		time.Sleep(25 * time.Millisecond)
+	}
+	fmt.Println("Stopping the writing")
 }
-
